dc7d073c339b616fef3a4fddccab2b0dcf26f50b
James Park <woghd8503@naver.com>
2020-10-08 21:48:39 +0900

    ARcamping(ARcore) 3Dsound스피커

---
+++ csSpeakerOnOff.cs
@@ -0,0 +1,70 @@
+﻿using GoogleARCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csSpeakerOnOff : MonoBehaviour
+{
+    Camera arCamera;        // ARCamera 게임오브젝트에 포함된 Camera컴포넌트를 얻어오기 위한 변수
+    AudioSource audioSource;      // 이 소스가 포함된 GameObject에 함께 포함된 AusioSource컴포넌트를 얻어오기 위한 변수
+    bool isOn = false;      // true면 소리가 발생, false면 소리가 꺼짐
+    public Object[] mats;   // 2개의 머티리얼을 외부에서 추가, 0 index는 소리가 꺼졌을 때 머티리얼, 1 index는 소리가 켜졌을 때 머티리얼
+    Renderer render;        // 머티러얼을 적용하기 위해 Mesh Render 컴포넌트를 얻어오기 위한 변수
+    public Object pointLightResource;   // 포인트 라이트 프리팹
+    GameObject pointLightObj = null;    // 포인트 라이트 프리팹으로 생성한 GameObject
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();        // 이 GameObject에 포함된 AudioSource 컴포넌트를 얻어옴
+        arCamera = GameObject.Find("First Person Camera").GetComponent<Camera>(); // 현재 씬에서 "First Person Camera" 이름의 GameObject를 찾아서 담아라
+        render = GetComponent<Renderer>();          // 이 GameObject에 포함된 Renderer 컴포넌틀 얻어옴
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0) return;      // 터치가 발생하지 않았다면 더 이상 아래를 진행하지 말 것
+
+        Touch touch = Input.GetTouch(0);        // 첫 번째 터치 정보 추출
+
+        Vector3 mousePosition = Input.mousePosition;    // 터치한 위치를 얻는다
+        Ray ray = arCamera.ScreenPointToRay(mousePosition);     // ARCamera위치에서 광선을 터치한 위치로 발사한 정보를 담을 객체
+        RaycastHit hit;                                 // ray와 충돌한 GameObject의 정보가 담기는 객체
+
+        Debug.Log("-------------- " + mousePosition.ToString());
+
+        // 터치가 시작되었고 And 카메라로부터 발사한 광선에 충돌한 GameObject가 있다면
+        if (touch.phase == TouchPhase.Began && Physics.Raycast(ray, out hit))   
+        {
+            Debug.Log("-------------- SpeakerOnOff - Touch");
+
+            if (hit.collider.tag != "Speaker") return;              // 충돌한 GameObject가 "Speaker"태그가 아니라면 더이상 진행하지 않고 return한다
+            if (hit.collider.gameObject != this.gameObject) return; // 충돌한 GameObject와 현재 GameObject가 같지 않으면 더이상 진행하지 않고 return한다
+
+            if (isOn)               // 현재 소리가 켜진 상태라면
+            {
+                Debug.Log("-------------- SpeakerOnOff - Speaker - isOn");
+                audioSource.Stop();       // 소리를 꺼라
+                isOn = false;       // 소리가 꺼졌다는 상태를 저장
+                render.material = (Material)mats[1];    // 소리가 꺼졌을 때 머티리얼을 적용
+
+                // 포인트 라이트 GameObject 생성할 위치를 현재 오브젝트의 위치보다 y축으로 10정도 위에 위치시킨다
+                Vector3 pointPosition = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z);
+                // 포인트 라이트 프리팹으로 포인트 라이트 GameObject를 생성하고, pointPosition위치와 이 GameObject의 각도와 일치시킨다
+                pointLightObj = (GameObject)Instantiate(pointLightResource, pointPosition, transform.rotation);
+            }
+            else               // 현재 소리가 꺼진 상태라면
+            {
+                Debug.Log("-------------- SpeakerOnOff - Speaker - isOn");
+                audioSource.Play();   // 현재 소리를 다시 켜라
+                isOn = true;    // 소리가 켜졌다는 상태를 저장
+                render.material = (Material)mats[0];    // 소리가 켜졌을 때 머티리얼을 적용
+                if (pointLightObj != null)              // 포인트 라이트 GameObject가 생성되어있다면
+                {
+                    Destroy(pointLightObj);             // 포인트 라이트 GameObject를 제거해라
+                    pointLightObj = null;               // 포인트 라이트 GameObject 저장 변수에 null을 저장한다
+                }
+            }
+        }
+    }
+}
---
+++ csTouchMgr.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCoreInternal;
+
+public class csTouchMgr : MonoBehaviour
+{
+    private Camera ARCamera;    //ARCore 카메라
+    public Object placeObject; //터치 시 평면에 생성할 프리팹
+
+    private void Start()
+    {
+        //ARCore Device 프리팹 하위에 있는 카메라를 찾아서 변수에 할당
+        ARCamera = GameObject.Find("First Person Camera").GetComponent<Camera>();
+        Debug.Log("--------------ARCamera : " + ARCamera.ToString());
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount == 0) return;          // 터치가 이루어지지 않았다면 더 이상 진행하지 말고 return해라
+
+        // 첫 번째 터치 정보 추출
+        Touch touch = Input.GetTouch(0);
+
+        Vector3 mousePosition = Input.mousePosition;            // 현재 터치한 위치를 얻는다
+        Ray ray = ARCamera.ScreenPointToRay(mousePosition);     // ARCamera로부터 발사한 광선 정보
+        RaycastHit rHit;                                        // 광선이 부딪히는 GameObject 정보
+
+        Debug.Log("-------------- " + mousePosition.ToString());
+
+        // 터치가 발생하였고 And 광선과 부딪힌 GameObject가 존재한다면
+        if (touch.phase == TouchPhase.Began && Physics.Raycast(ray, out rHit))
+        {
+            // 부딪힌 GameObject의 Tag가 Speaker라면 더 이상 진행하지 말고 return 해라
+            if (rHit.collider.tag == "Speaker")
+                return;
+        }
+
+        //ARCore에서 제공하는 RaycastHit와 유사한 구조체
+        TrackableHit hit;
+
+        //검출 대상을 평면 또는 Feature Point로 한정
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon | TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        //터치한 지점으로 레이 발사하고 And 터치한 위치에서 광선을 발사했을 때 평면이나 특징점(Feature Point)과 충돌했다면
+        if (touch.phase == TouchPhase.Began && Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            Debug.Log("-------------- Touch");
+            //객체를 고정할 앵커를 생성
+            var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            //placeObject에 담긴 Prefab 객체를 생성
+            GameObject obj = (GameObject)Instantiate(placeObject, hit.Pose.position, Quaternion.identity, anchor.transform);
+
+            // 생성한 객체가 사용자 쪽을 바라보도록 회전값 계산
+            var rot = Quaternion.LookRotation(ARCamera.transform.position - hit.Pose.position);
+
+            // 사용자 쪽 회전값 적용
+            obj.transform.rotation = Quaternion.Euler(ARCamera.transform.position.x, rot.eulerAngles.y, ARCamera.transform.position.z);
+        }        
+    }
+}
